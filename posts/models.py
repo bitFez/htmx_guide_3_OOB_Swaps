718fbbce183111ccadbfbfb808c617424ebc7780
learnICT <kibrisli7@gmail.com>
2023-02-10 13:06:08 +0000

    example completed

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     post_author = models.ForeignKey(User, on_delete=models.CASCADE)  # loads the USER linked to the profile from django AUTH
     post_content = models.TextField(null=True, blank=True)
-    likes = models.ManyToManyField(User, blank=True, related_name="collected_votes")
+    likes = models.ManyToManyField(User, blank=True, related_name="likers")
 
     # def __str__(self):
     #     return self.id
